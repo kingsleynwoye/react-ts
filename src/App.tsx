f3814b56b7ebf9ba4adb38fcdd10ba7657755d34
Chidubem <kingsleychidubem042@gmail.com>
2021-10-04 11:21:38 +0100

    Rendered dummy data in the todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+
+import Todos from "./components/Todos";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Todos items={["Learn JavaScript", "Learn React", "Learn TypeScript"]} />
+    </div>
+  );
 }
 
 export default App;
